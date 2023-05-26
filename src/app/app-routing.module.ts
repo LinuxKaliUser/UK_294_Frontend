89a8a339bc5b85b38716ab67b0cc5a396a946d81
pc110 <arunan.nagarajah@bbzbl-it.ch>
2023-05-26 09:47:19 +0200

    Added Login &Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NoAccessComponent } from './pages/no-access/no-access.component';
+import { AppLoginComponent } from './components/app-login/app-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: DashboardComponent},
+{path: 'dashboard', component: DashboardComponent},
+{path: 'noaccess', component: NoAccessComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,128 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatTableModule} from '@angular/material/table';
+import {AuthConfig, OAuthModule, OAuthStorage} from 'angular-oauth2-oidc';
+import {AppAuthService} from './service/app.auth.service';
+import {AppAuthGuard} from './guard/app.auth.guard';
+import {LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {IsInRoleDirective} from './dir/is.in.role.dir';
+import {IsInRolesDirective} from './dir/is.in.roles.dir';
+import {MatButtonModule} from '@angular/material/button';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {NoAccessComponent} from './pages/no-access/no-access.component';
+import {AppLoginComponent} from './components/app-login/app-login.component';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule, HttpClientXsrfModule} from '@angular/common/http';
+import {HttpXSRFInterceptor} from './interceptor/http.csrf.interceptor';
+import {AppHeaderComponent} from './components/app-header/app-header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+import {environment} from '../environments/environment';
+import { PersonComponent } from './pages/person/person.component';
+import { TaskComponent } from './pages/task/task.component';
+import { MealComponent } from './pages/meal/meal.component';
+
+export const authConfig: AuthConfig = {
+  issuer: 'http://localhost:8080/realms/ILV',
+  requireHttps: false,
+  redirectUri: environment.frontendBaseUrl,
+  postLogoutRedirectUri: environment.frontendBaseUrl,
+  clientId: 'zuffallsgeneratorApp',
+  scope: 'openid profile roles offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+  requestAccessToken: true,
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  silentRefreshTimeout: 500,
+  clearHashAfterLogin: true,
+};
+
+export function storageFactory(): OAuthStorage {
+  return sessionStorage;
+}
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IsInRoleDirective,
+    IsInRolesDirective,
+    DashboardComponent,
+    NoAccessComponent,
+    AppLoginComponent,
+    AppHeaderComponent,
+    PersonComponent,
+    TaskComponent,
+    MealComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'XSRF-TOKEN',
+      headerName: 'X-XSRF-TOKEN'
+    }),
+    MatSliderModule,
+    MatTableModule,
+    OAuthModule.forRoot({resourceServer: {sendAccessToken: true}}),
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatMomentDateModule
   ],
-  providers: [],
+  providers: [
+    {provide: AuthConfig, useValue: authConfig},
+    {provide: HTTP_INTERCEPTORS, useClass: HttpXSRFInterceptor, multi: true},
+    {
+      provide: OAuthStorage, useFactory: storageFactory
+    },
+    AppAuthGuard,
+    Location, {provide: LocationStrategy, useClass: PathLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(authService: AppAuthService) {
+    authService.initAuth().finally();
+  }
+}
