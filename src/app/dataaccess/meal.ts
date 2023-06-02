ba694e170e7d205bbd017207bb60a8c3ffa6b2fb
pc110 <arunan.nagarajah@bbzbl-it.ch>
2023-06-02 10:11:15 +0200

    deleted usless test and changed entity

--- src/app/dataaccess/meal.ts
+++ src/app/dataaccess/meal.ts
@@ -4,7 +4,7 @@ export class Meal {
   public id!: number;
   public designation = '';
   public sequence: number | undefined;
-  public cost : Big | undefined;
+  public cost : number | undefined;
   public dateSetting ='';
   public remarks = '';
 
--- src/app/dataaccess/task.ts
+++ src/app/dataaccess/task.ts
@@ -4,7 +4,7 @@ export class Task {
   public id!: number;
   public designation = '';
   public sequence: number | undefined;
-  public taskDuration : Time | undefined ;
+  public taskDuration : number | undefined ;
   public dateSetting ='';
   public remarks = '';
 
