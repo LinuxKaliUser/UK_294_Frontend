873653fd9282a2a2073c36fbc9bb87ced81e7cf1
pc110 <arunan.nagarajah@bbzbl-it.ch>
2023-06-02 14:41:34 +0200

    Last check

--- src/app/dataaccess/team.ts
+++ src/app/dataaccess/team.ts
@@ -3,7 +3,7 @@ import { Person } from "./person";
 export class Team {
   public id!: number;
   public name = '';
-  public totalMembers: number | undefined;
+  public totalMembers=0;
   public persons : Person[] | undefined ;
   public remarks = '';
 }
